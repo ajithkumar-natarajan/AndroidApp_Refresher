a8871da1f5bf71b50d350a17c24118338c4edd05
najith97 <najit97@gmail.com>
2020-02-01 23:59:57 -0400

    Done with main activity layout and src

---
+++ app/src/main/java/com/work/refresherapplication/MainActivity.java
@@ -0,0 +1,28 @@
+package com.work.refresherapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    public final static String EXTRA_MESSAGE = "com.work.refresherapplication.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+}
